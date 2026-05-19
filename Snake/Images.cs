ec7270a69044387a5b31b46ecf35c54856724a14
T J Byars <tombyars1@gmail.com>
2026-05-19 19:53:49 +0100

    General implementation complete, game is technically playable

---
+++ Snake/Images.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+
+namespace Snake
+{
+    public static class Images
+    {
+        public readonly static ImageSource Empty = LoadImage("empty.png");
+        public readonly static ImageSource Body = LoadImage("snakeBody.png");
+        public readonly static ImageSource Head = LoadImage("snakeFace.png");
+        public readonly static ImageSource Food = LoadImage("food.png");
+        public readonly static ImageSource DeadBody = LoadImage("deadSnakeBody.png");
+        public readonly static ImageSource DeadHead = LoadImage("deadSnakeFace.png");
+
+        private static ImageSource LoadImage(string filename)
+        {
+            return new BitmapImage(new Uri($"Assets/{filename}", UriKind.Relative));
+        }
+    }
+}
---
+++ Snake/Position.cs
@@ -0,0 +1,41 @@
+﻿namespace Snake
+{
+    public class Position
+    {
+        public int Row { get; }
+        public int Column { get; }
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public Position Translate(Direction dir)
+        {
+            return new Position(Row + dir.RowOffset, Column + dir.ColOffset);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Position position &&
+                   Row == position.Row &&
+                   Column == position.Column;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Row, Column);
+        }
+
+        public static bool operator ==(Position left, Position right)
+        {
+            return EqualityComparer<Position>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Position left, Position right)
+        {
+            return !(left == right);
+        }
+    }
+}
